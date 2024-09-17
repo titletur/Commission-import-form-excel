c6e1b8bb340fcb096d9586a63b0ba314f8ea9256
titletur <titletur@gmail.com>
2024-09-17 09:02:22 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -50,7 +50,11 @@
                                             Register
                                         </a>
                                     @endif
-                                @endauth
+                                @endauth 
+                                        <a href="{{ asset('Commission-BigC-Manual.pdf') }}" target="_blink" class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                        >
+                                            README
+                                        </a>
                             </nav>
                         @endif
                     </header>
