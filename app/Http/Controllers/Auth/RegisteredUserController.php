446de38820af8ed321b01ef423d78eb13985df9e
titletur <titletur@gmail.com>
2024-09-18 15:19:32 +0700

    Update RegisteredUserController.php

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -42,7 +42,7 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
             'department' => $request->department,
-            'permissions' => 'users',
+            'permissions' => json_encode(['View_data']),
         ]);
 
         event(new Registered($user));
