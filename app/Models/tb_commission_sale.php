62b9766c5efbf29cfe2c97a93778a73cacdd98b1
titletur <titletur@gmail.com>
2024-09-20 09:41:57 +0700

    Update tb_commission_sale.php

--- app/Models/tb_commission_sale.php
+++ app/Models/tb_commission_sale.php
@@ -11,7 +11,7 @@ class tb_commission_sale extends Model
     protected $table = 'tb_commission_sale';
 
     protected $fillable = [
-        'is_sale','as_of_month', 'as_of_year','target', 'achieve', 'base_com', 'com_sale','sale_tv',
+        'is_sale','as_of_month', 'as_of_year','target', 'add_total' , 'achieve', 'base_com', 'com_sale','sale_tv',
         'unit_tv','sale_av','unit_av','sale_ha','unit_ha','sale_out','extra_sale_out','extra_unit',
         'extra_avg','other','total','remark'
     ];
