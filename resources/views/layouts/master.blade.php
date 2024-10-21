8e99b5dc859c6fe094d46deeea281a846be65569
titletur <titletur@gmail.com>
2024-10-21 10:17:21 +0700

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
-    <link rel="icon" href="{{ asset('bigc.jpg') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('icon.jpg') }}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/cssfont.css') }}">
     <link rel="stylesheet" href="{{ asset('css/chosen.min.css') }}">
