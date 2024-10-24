462a0b05060130a3a16ad6d4d265f8b441578986
titletur <titletur@gmail.com>
2024-10-24 16:10:53 +0700

    Update product.php

--- app/Models/product.php
+++ app/Models/product.php
@@ -10,8 +10,7 @@ class Product  extends Model
     use HasFactory;
     protected $table = 'tb_product';
     protected $fillable = [
-        'suppliercode', 'division', 'department', 'subdepartment', 
-        'pro_Class', 'sub_pro_class', 'barcode', 'article', 
-        'article_name', 'brand', 'pro_model','type_product','price','price_vat','com'
+        'supplier_number', 'item_number', 'barcode', 'item_des', 
+        'type_product', 'pack_type', 'price_vat', 'com', 'status_product'
     ];
 }
