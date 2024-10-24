63baeea451cb0bcbf4d17bec8b82129ab3d147e7
titletur <titletur@gmail.com>
2024-10-24 16:10:48 +0700

    Update commission.php

--- app/Models/commission.php
+++ app/Models/commission.php
@@ -11,8 +11,39 @@ class Commission extends Model
     protected $table = 'tb_commission';
 
     protected $fillable = [
-        'suppliercode', 'store_id','type_store', 'as_of_month', 'as_of_year', 'pro_model','type_product',
-        'sale_amt','sale_amt_vat','sale_qty','id_pc','type_pc','com'
+        'supplier_number', 'store_id','type_store', 'as_of_month', 'as_of_year', 'item_number','type_product',
+        'sale_total','sale_qty','id_pc','type_pc','com',
+        'day1',
+        'day2',
+        'day3',
+        'day4',
+        'day5',
+        'day6',
+        'day7',
+        'day8',
+        'day9',
+        'day10',
+        'day11',
+        'day12',
+        'day13',
+        'day14',
+        'day15',
+        'day16',
+        'day17',
+        'day18',
+        'day19',
+        'day20',
+        'day21',
+        'day22',
+        'day23',
+        'day24',
+        'day25',
+        'day26',
+        'day27',
+        'day28',
+        'day29',
+        'day30',
+        'day31',
     ];
 }
 
